11a706cf64d417765d85fbb908e6e2408ac0a894
homegirl-fxd <39789543+homegirl-fxd@users.noreply.github.com>
2019-01-04 10:14:57 +0800

    190104

--- src/main/java/com/xiaodan/demo/dao/impl/AccountDaoImpl.java
+++ src/main/java/com/xiaodan/demo/dao/impl/AccountDaoImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 /**
- * Created by fengxiaodan on 2018/9/19.
+ * Created by fengxiaodan on 2018/9/19.       修改
  */
 
 /**
